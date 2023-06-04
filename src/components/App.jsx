1a8f0ff62358e78928c358f6a6b503ce3abaeae3
Evgeniy <evgnec@gmail.com>
2023-06-04 20:08:47 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,10 +6,16 @@ import { nanoid } from 'nanoid'
 // import Notification from './Notification/Notification';
 
 export class App extends React.Component {
-  state = {
-  contacts: [],
-    name: '',
-  id: '',
+state = {
+  contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+  filter: '',
+  name: '',
+  number: ''
 }
   // this.state.id = nanoid();
 
@@ -31,10 +37,15 @@ export class App extends React.Component {
   pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
   title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required />
-           <button type="submit"></button>
+           <button type="submit">Add contacts</button>
         </form>
-
-
+<h2>Contacts</h2>
+<input
+  type="text"
+  name="findName"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required />
       </>
     );
   }
